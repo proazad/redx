51077f0a0b0d51f94fed534cbca0ed77aa23d812
proazad <proazadhossain7595@gmail.com>
2023-09-15 14:22:13 +0300

    Services Section Design and Data Load Complate

--- src/Components/Services/Service/Service.jsx
+++ src/Components/Services/Service/Service.jsx
@@ -1,9 +1,19 @@
-const Service = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+import PropTypes from "prop-types";
+const Service = ({ service }) => {
+  const { title, image, description } = service;
+  return (
+    <div className="card rounded-none">
+      <figure>
+        <img src={image} alt={title} />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{title}</h2>
+        <p> {description}</p>
+      </div>
+    </div>
+  );
 };
-
+Service.propTypes = {
+  service: PropTypes.object,
+};
 export default Service;
--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,11 +1,18 @@
+import PropTypes from "prop-types";
 import Service from "./Service/Service";
-const Services = ({services}) => {
-    return (
-        <div>
-            <h1 className='text-3xl'>সার্ভিস সমূহ</h1>
-            <Service></Service>
-        </div>
-    );
+const Services = ({ services }) => {
+  return (
+    <div className="container mx-auto my-16">
+      <h1 className="text-4xl font-bold text-center">সার্ভিস সমূহ</h1>
+      <div className="my-10 grid grid-cols-1 lg:grid-cols-4 gap-10">
+        {services.map((service) => (
+          <Service key={service.id} service={service}></Service>
+        ))}
+      </div>
+    </div>
+  );
 };
-
+Services.propTypes = {
+  services: PropTypes.array,
+};
 export default Services;
