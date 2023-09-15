e0273fe2f58f8b1650d774d391d781200f3c5cc9
proazad <proazadhossain7595@gmail.com>
2023-09-15 10:44:58 +0300

    Initial Setup Complete Using Tools React, Taildwind CSS, DaisyUI, Hind Siliguri google font and

---
+++ Backup.js
@@ -0,0 +1,4 @@
+// import PropTypes from 'prop-types';
+// MyComponent.propTypes = {
+//     optionalArray: PropTypes.array
+// }
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-semibold text-red-700">Redx</h1>
+    </>
+  )
+}
+
+export default App
